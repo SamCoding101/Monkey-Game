175282b3e26c881fb5eff75992469648c8907884
SamCoding101 <69662497+SamCoding101@users.noreply.github.com>
2020-08-25 19:51:28 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,7 +28,7 @@ createCanvas(windowWidth,windowHeight);
 
   ground = createSprite(width - 1580,height - 400, 2500, 10);
   ground.velocityX = -3;
-  ground.x = ground.width / 1.5;
+  ground.x = ground.width;
 
   foodGroup = createGroup();
   obstacleGroup = createGroup();
